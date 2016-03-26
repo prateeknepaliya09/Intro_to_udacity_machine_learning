c06a91fc4bdb7a3f8f8167b1efc53732260ca63f
Prateek Nepaliya <prateeknepalia9@gmail.com>
2016-03-26 23:30:57 +0530

    Updated

--- feature_selection/find_signature.py
+++ feature_selection/find_signature.py
@@ -8,8 +8,8 @@ numpy.random.seed(42)
 ### The words (features) and authors (labels), already largely processed.
 ### These files should have been created from the previous (Lesson 10)
 ### mini-project.
-words_file = "../text_learning/your_word_data.pkl" 
-authors_file = "../text_learning/your_email_authors.pkl"
+words_file = "../text_learning/word_data.pkl" 
+authors_file = "../text_learning/email_authors.pkl"
 word_data = pickle.load( open(words_file, "r"))
 authors = pickle.load( open(authors_file, "r") )
 
@@ -38,6 +38,15 @@ labels_train   = labels_train[:150]
 
 
 ### your code goes here
+from sklearn.tree import DecisionTreeClassifier
 
+clf = DecisionTreeClassifier()
+clf.fit(features_train, labels_train)
+#cross_validation.cross_val_score(clf, features_train, labels_train  )
+pred = clf.predict(features_test)
 
+from sklearn.metrics import accuracy_score
+accuracy = accuracy_score(labels_test, pred)
+print accuracy
+#print DecisionTreeClassifier.score()
 
--- text_learning/vectorize_text.py
+++ text_learning/vectorize_text.py
@@ -9,17 +9,13 @@ sys.path.append( "../tools/" )
 from parse_out_email_text import parseOutText
 
 """
-    Starter code to process the emails from Sara and Chris to extract
-    the features and get the documents ready for classification.
-
-    The list of all the emails from Sara are in the from_sara list
+    starter code to process the emails from Sara and Chris to extract
+    the features and get the documents ready for classification
+    the list of all the emails from Sara are in the from_sara list
     likewise for emails from Chris (from_chris)
-
-    The actual documents are in the Enron email dataset, which
-    you downloaded/unpacked in Part 0 of the first mini-project. If you have
-    not obtained the Enron email corpus, run startup.py in the tools folder.
-
-    The data is stored in lists and packed away in pickle files at the end.
+    the actual documents are in the Enron email dataset, which
+    you downloaded/unpacked in Part 0 of the first mini-project
+    the data is stored in lists and packed away in pickle files at the end
 """
 
 
@@ -32,32 +28,40 @@ word_data = []
 ### temp_counter is a way to speed up the development--there are
 ### thousands of emails from Sara and Chris, so running over all of them
 ### can take a long time
-### temp_counter helps you only look at the first 200 emails in the list so you
-### can iterate your modifications quicker
+### temp_counter helps you only look at the first 200 emails in the list
 temp_counter = 0
-
 
 for name, from_person in [("sara", from_sara), ("chris", from_chris)]:
     for path in from_person:
         ### only look at first 200 emails when developing
         ### once everything is working, remove this line to run over full dataset
-        temp_counter += 1
+        # temp_counter += 1
         if temp_counter < 200:
             path = os.path.join('..', path[:-1])
             print path
             email = open(path, "r")
 
             ### use parseOutText to extract the text from the opened email
+            content = parseOutText(email)
 
             ### use str.replace() to remove any instances of the words
             ### ["sara", "shackleton", "chris", "germani"]
+            stop_words = ["sara", "shackleton", "chris", "germani", "sshacklensf", "cgermannsf", "houectect"]
+
+            for w in stop_words:
+                content = content.replace(w, "")
 
             ### append the text to word_data
+            word_data.append( content )
+
+            if name == "sara":
+                from_data.append(0)
+            else:
+                from_data.append(1)
 
             ### append a 0 to from_data if email is from Sara, and 1 if email is from Chris
-
-
             email.close()
+
 
 print "emails processed"
 from_sara.close()
@@ -68,8 +72,11 @@ pickle.dump( from_data, open("your_email_authors.pkl", "w") )
 
 
 
-
-
 ### in Part 4, do TfIdf vectorization here
 
+from sklearn.feature_extraction.text import TfidfVectorizer
+vectorizer = TfidfVectorizer(stop_words="english")
 
+vectorizer.fit_transform(word_data)
+print "#feature names: ", len(vectorizer.get_feature_names())
+print vectorizer.get_feature_names()[33614]
